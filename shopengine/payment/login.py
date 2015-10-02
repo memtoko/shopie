a8fd32d4351a861957cb3967f2e396464a8d1abd
Memtoko <sales@memtoko.com>
2015-10-02 20:09:26 +0700

    initial implementation for checkout step

--- shopengine/payment/login.py
+++ shopengine/payment/login.py
@@ -2,18 +2,23 @@ from django.utils.decorators import method_decorator
 from django.utils.translation import ugettext_lazy as _
 from django.views.generic.edit import FormView
 from django.contrib.auth.models import AnonymousUser
-
+from django.contrib.auth.forms import AuthenticationForm
 from registration.forms import RegistrationForm
 from registration import signals as registration_signal
 
 from shopengine.decorators import cart_nonempty_required
 from .checkout import CheckoutStepMixin
+from django.contrib.auth import login as auth_login
 
 class CheckoutLoginStep(CheckoutStepMixin, FormView):
     identifier = "login"
     title = _("Login or Create")
     template_name = "shopengine/checkout/login.html"
-    form_class = RegistrationForm
+    form_class = AuthenticationForm
+
+    def form_valid(self, request, form):
+        auth_login(request, form.get_user())
+        return super(CheckoutLoginStep, self).form_valid(request, form)
 
     def is_valid(self):
         return self.request.user and not isinstance(self.request.user, AnonymousUser)
--- shopengine/views/checkout.py
+++ shopengine/views/checkout.py
@@ -1,9 +1,15 @@
-from django.shortcuts import render
+import urllib
+
+from django.shortcuts import render, redirect
 from django.http import Http404
+from django.core.urlresolvers import reverse
+from django.views.generic import View
+from django.utils.decorators import method_decorator
 
 from shopengine.models import Order
 from shopengine.forms.checkout import CheckoutForm
 from shopengine.payment.checkout import CheckoutProcess
+from shopengine.decorators import cart_nonempty_required
 
 def checkout_thankyou(request, order_key):
 	"""View for thank you page."""
@@ -13,3 +19,24 @@ def checkout_thankyou(request, order_key):
         raise Http404()
     else:
         return render(request, "shopengine/payment/thank_you.html", {'order': order})
+
+class BaseCheckoutView(View):
+    step_specs = []
+
+    @method_decorator(cart_nonempty_required('cart'))
+    def dispatch(self, request, *args, **kwargs):
+        process = CheckoutProcess(
+            step_specs=self.step_specs,
+            step_kwargs=dict(request=self.request, args=self.args, kwargs=self.kwargs)
+        )
+        step_id = kwargs.get('step')
+        if step_id == 'reset':
+            process.reset()
+            return redirect(reverse('checkout'))
+
+        current_step = process.prepare_current_step(step_id)
+        if not current_step.final and current_step.identifier != step_id:
+            url = reverse("checkout", kwargs={"step": current_step.identifier})
+            params = ("?" + urllib.parse.urlencode(request.GET)) if request.GET else ""
+            return redirect(url + params)
+        return current_step.dispatch(request, *args, **kwargs)
