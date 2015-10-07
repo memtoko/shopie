d09035a02677a626e6fefa49c0b2e88fddb8a1ac
Syaiful Bahri <sbahri@indo-soft.com>
2015-10-07 14:22:22 +0700

    move url view for payment backend
    Because before this, its live on global.

--- shopengine/payment/backends/urls.py
+++ shopengine/payment/backends/urls.py
@@ -7,7 +7,7 @@ urlpatterns = patterns('')
 # For every backend defined in the backend pool, load all the URLs it defines
 # in its get_urls() method.
 for backend in payment_backend_pool.get_backend_list():
-    regexp = '^%s/' % backend.url_namespace
+    regexp = '^payment%s/' % backend.url_namespace
     urls = backend.get_urls()
     pattern = patterns('',
         (regexp, include(backend.get_urls()))
