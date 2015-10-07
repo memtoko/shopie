831690db40f467719e89b3361e18c24c5d5a86e0
Syaiful Bahri <sbahri@indo-soft.com>
2015-10-07 14:34:47 +0700

    restructure our url routing

--- shopengine/urls/checkout.py
+++ shopengine/urls/checkout.py
@@ -1,8 +1,10 @@
 from django.conf.urls import patterns, url
 
 from shopengine.views.checkout import CheckoutView
+from shopengine.payment.backends.urls import urlpatterns as payment_backends_urls
 
 urlpatterns = patterns('',
         url(r'^checkout/$', CheckoutView.as_view(), name='checkout'),
-        url(r'^checkout/(?P<step>.+)/$', CheckoutView.as_view(), name='checkout')
+        url(r'^checkout/(?P<step>.+)/$', CheckoutView.as_view(), name='checkout'),
+        url(r'^pay/', include(payment_backends_urls))
     )
