b35dca4cda6bbcaa83134d5ff188c4668bf8f6f5
Syaiful Bahri <sbahri@indo-soft.com>
2015-10-13 15:57:32 +0700

    style for our app

--- shopie/assets/app/models/user.js
+++ shopie/assets/app/models/user.js
@@ -1,7 +1,7 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  firstName: DS.attr('string'),
-  lastName: DS.attr('string'),
-  email: DS.attr('string')
+    firstName: DS.attr('string'),
+    lastName: DS.attr('string'),
+    email: DS.attr('string')
 });
