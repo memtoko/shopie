0ba0b933de9bbdb3b44f74521180dde59ade8790
Syaiful Bahri <sbahri@indo-soft.com>
2015-10-15 11:48:06 +0700

    clean up files

--- shopie/assets/app/adapters/base.js
+++
@@ -1,7 +0,0 @@
-import DS from 'ember-data';
-import shopiePaths from 'shopie/utils/shopie-paths';
-
-export default DS.JSONAPIAdapter.extend({
-    host: window.location.origin,
-    namespace: shopiePaths().apiRoot.slice(1)
-});
--- shopie/assets/app/models/cart.js
+++
@@ -1,9 +0,0 @@
-import DS from 'ember-data';
-
-export default DS.Model.extend({
-    quantity: DS.attr('integer'),
-    subtotalPrice: DS.attr('string'),
-    totalPrice: DS.attr('string'),
-    extraPrice: DS.attr(),
-    user: DS.belongsTo('user')
-});
